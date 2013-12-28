e79a116cb068a7c8b4ec42d0d7949c90dca03fb2
shinesong123 <shinesongjingzhe@gmail.com>
2013-12-28 03:00:06 -0500

    Update Linked List Cycle.java

--- Linked List Cycle.java
+++ Linked List Cycle.java
@@ -1,5 +1,4 @@
-Linked List Cycle 
-Given a linked list, determine if it has a cycle in it.
+/**Given a linked list, determine if it has a cycle in it.
 
 Follow up:
 Can you solve it without using extra space?
