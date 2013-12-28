e37bbe76a823b454bd0b346c0daa7083f86d1330
shinesong123 <shinesongjingzhe@gmail.com>
2013-12-28 03:00:38 -0500

    Update Linked List Cycle.java

--- Linked List Cycle.java
+++ Linked List Cycle.java
@@ -1,7 +1,8 @@
-/**Given a linked list, determine if it has a cycle in it.
-
-Follow up:
-Can you solve it without using extra space?
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ * Follow up:
+ * Can you solve it without using extra space?
+*/
 
 /**
  * Definition for singly-linked list.
