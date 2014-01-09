77d4219a3dd516b249496aaa3588a2c3e3058658
shinesong123 <shinesongjingzhe@gmail.com>
2014-01-09 02:53:18 -0500

    Update Pow(x, n).java

--- Pow(x, n).java
+++ Pow(x, n).java
@@ -11,7 +11,8 @@ the idea is do x^n/2, x^n/4...
    if n is odd && n>0, power(x,n/2)*power(x,n/2)*x
                && n<0, power(x,n/2)*power(x,n/2)*(1/x)
 2. end condition: n==0, x=1; could also be n==1
-
+3. JAVA负数:-7/2=-3; 21/-5=-4;-21/-8=2; 正常触发
+            -21%-8=-5; 21%-5=1; 被除数有多少完整的除数,剩下什么是什么,不求补
 
 */
 public class Solution {
