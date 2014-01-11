e91dd94b4af5d6bdc220f71ecbf4bb2481acf857
shinesong123 <shinesongjingzhe@gmail.com>
2014-01-11 02:17:29 -0500

    Update Maximum Subarray.java

--- Maximum Subarray.java
+++ Maximum Subarray.java
@@ -9,6 +9,24 @@ the contiguous subarray [4,−//O(n) 遍历一遍
 // 当sum>max时,说明发现了更大的subarray序列, 此时更新max
 
 // 对于新出现的一个数,如果之前subarray的sum>0, 考虑加入,判断一下加入后是不是比max大; 如果sum<0, 加入对数本身来说没有任何帮助,只会更小,还不如就自己,或自己带后面的
+
+
+/*
+Use divide and conquer
+
+Any subarray A[i,...,j] is:
+1) Entirely in A[low,mid]
+2) Entirely in A[mid+1, high]
+3) in both
+
+1) and 2) could be do recursively; 
+3) is find maximum subarrays crosses midpoint, which is find max A[i,..mid]+max A[mid+1,..,j]
+
+Time: T(n)=2T(n/2)+O(n)=O(nlgn)
+*/
+
+
+
 public class Solution {
     public int maxSubArray(int[] A) {
         
